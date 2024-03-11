372cd1008fdb4dc25914b440745576f667295928
Nasrin <nasrinbegump@gmail.com>
2024-03-11 13:38:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from browser import document
+
+document <= "Hello World!"
+document <= "Hello World!"
+document <= "Hello World!"
+
+
+import pandas as pd
+
+# Creating Series of
+# programming languages
+s = pd.Series(['C', 'C++', 'Java',
+               'Python', 'Perl', 'Ruby',
+               'Julia'])
+
+print(s)
+
+
